c9ab2d01c58d713a6720d08663cd2694c188017b
Patton M Finley <finleyp@mail.gvsu.edu>
2018-09-26 16:53:33 -0400

    updating gitignore

--- build/app/generated/source/r/debug/android/support/mediacompat/R.java
+++ build/app/generated/source/r/debug/android/support/mediacompat/R.java
@@ -93,6 +93,7 @@ public final class R {
         public static final int right_icon = 0x7f070060;
         public static final int right_side = 0x7f070061;
         public static final int status_bar_latest_event_content = 0x7f07007d;
+        public static final int tag_transition_group = 0x7f070081;
         public static final int text = 0x7f070083;
         public static final int text2 = 0x7f070084;
         public static final int time = 0x7f070087;
--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /home/braedin/development/flutter/bin/cache/artifacts/engine/linux-x64/frontend_server.dart.snapshot
+frontend_server.d: C:\src\flutter\bin\cache\artifacts\engine\windows-x64\frontend_server.dart.snapshot
